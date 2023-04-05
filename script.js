351b656642d3c3e8eb61448aa670c1d2498d45f4
SABITABRATA BHATTACHARYA <120928064+Sabitabrata2014@users.noreply.github.com>
2023-04-05 11:24:46 +0000

    Modified script.js

--- script.js
+++ script.js
@@ -22,19 +22,37 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function goStore(params) {
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+
+  text.innerText = "You enter the store.";
+}
+
+function goCave() {
+  console.log("Going to cave");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon");
+}
+
+function buyHealth() {
   
 }
 
-function goCave(params) {
+function buyWeapon() {
   
 }
 
-function fightDragon(params) {
+function goTown() {
   
 }
-
-
 /* 
 Multiline 
 Comment
