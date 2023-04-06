b348cd9d724760015791e93ca83e6204dbf3ac53
SABITABRATA BHATTACHARYA <120928064+Sabitabrata2014@users.noreply.github.com>
2023-04-06 11:37:41 +0000

    edited script.js again

--- script.js
+++ script.js
@@ -80,10 +80,6 @@ function goCave() {
     update(locations[2]);
 }
 
-function fightDragon() {
-  console.log("Fighting dragon");
-}
-
 function buyHealth() {
   if(gold>=10){
   gold -= 10;
@@ -91,12 +87,41 @@ function buyHealth() {
   goldText.innerText = gold;
   healthText.innerText = health;
   }else{
-    text.innerText = "You do not have enough gold to buy health."
+    text.innerText = "You do not have enough gold to buy health.";
   }
 }
 
 function buyWeapon() {
+  if(currentWeapon < weapons.length-1){
+    if(gold>=30){
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a "+ newWeapon +".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: "+ inventory +".";
+    }else{
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  }else{
+    text.innerText = "You already have the most powerfull weapon.";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
   
+}
+
+function sellWeapon(){
+  if(inventory.length > 1){
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a "+currentWeapon+".";
+    text.innerText += " In your inventory you have: "+ inventory +".";
+  }else{
+    text.innerText += "Don't sell your only weapon!";
+  }
 }
 
 function fightSlime() {
@@ -107,6 +132,9 @@ function fightBeast() {
   
 }
 
+function fightDragon() {
+  console.log("Fighting dragon");
+}
 /* 
 Multiline 
 Comment
